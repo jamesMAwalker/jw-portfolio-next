825bc290006cf9b0707d1cc0bfcab6d797dc3531
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2021-12-27 16:16:35 +0700

    Contact Section functionality and styling. 1 Contact:   + Add light grey background to banner.   + Further refactoring of phrase sliders.   + Add copy functionality to email.   + Add tooltip to inform user of copy functionality.

--- components/home/05-contact.jsx
+++ components/home/05-contact.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import {
   contact,
@@ -7,57 +7,66 @@ import {
   line,
   lineOne,
   lineTwo,
-  phrase,
+  phrase as phraseStyle,
   blurb,
   email,
+  emailText,
+  emailToolTip,
+  toolTipSlider,
+  off
 } from '../../styles/home/05-contact.module.scss'
 
 export const Contact = () => {
+  // boolean for controlling tt slider position and therefore its content
+  const [sliderOff, setSliderOff] = useState(true)
+
+  const copyEmail = () => {
+    navigator.clipboard.writeText('me@jmswlkr.dev')
+    setSliderOff(false)    
+  }
+
   return (
     <section className={contact}>
       <h3 className={label}>Contact</h3>
       <div className={banners}>
-        <div className={`${line} ${lineOne}`}>
-          <div>
-            {Array.from({ length: 12 }).map((phrase) => (
-              <span className={phrase}>
-                Say Hello -&nbsp;
-              </span>
-            ))}
-          </div>
-          <div>
-            {Array.from({ length: 12 }).map((phrase) => (
-              <span className={phrase}>
-                Say Hello -&nbsp;
-              </span>
-            ))}
-          </div>
-        </div>
-        <div className={`${line} ${lineTwo}`}>
-          <div>
-            {Array.from({ length: 12 }).map((phrase) => (
-              <span className={phrase}>
-                Get in Touch -&nbsp;
-              </span>
-            ))}
-          </div>
-          <div>
-            {Array.from({ length: 12 }).map((phrase) => (
-              <span className={phrase}>
-                Get in Touch -&nbsp;
-              </span>
-            ))}
-          </div>
-        </div>
+        {['Get in Touch - ', 'Say Hello - '].map(
+          (phrase, idx) => {
+            const lineNum = idx === 0 ? lineOne : lineTwo
+
+            return (
+              <div className={`${line} ${lineNum}`}>
+                <a href='mailto:me@jmswlkr.dev'>
+                  {Array.from({ length: 24 }).map(
+                    (phraseChunk) => (
+                      <span className={phraseStyle}>
+                        {phrase}
+                      </span>
+                    )
+                  )}
+                </a>
+              </div>
+            )
+          }
+        )}
       </div>
       <p className={blurb}>
         <span>Currently available</span> for project work,
         but also seeking fulltime employment. Either way,
         let’s have a conversation.
       </p>
-      <a href='mailto:me@jmswlkr.devg' className={email}>
-        me@jmswlkr.dev
-      </a>
+      <div className={email} onClick={copyEmail} onMouseLeave={() => {
+        setTimeout(() => {
+          setSliderOff(true)
+        }, 200);
+      }}>
+        <div className={emailText}>me@jmswlkr.dev</div>
+        <div className={emailToolTip}>
+          <div className={`${toolTipSlider} ${sliderOff ? off : null}`}>
+            <span>click to copy</span>
+            <span>copied!</span>
+          </div>
+        </div>
+      </div>
     </section>
   )
 }
