331db47534dc954498864e91cde9452ffbc9dc04
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2021-12-30 11:37:03 +0700

    Fix button fonts.

--- pages/_app.js
+++ pages/_app.js
@@ -2,9 +2,7 @@ import '../styles/globals.scss'
 import '../styles/_mixins.scss'
 
 function MyApp({ Component, pageProps }) {
-  return (
-      <Component {...pageProps} />
-  )
+  return <Component {...pageProps} />
 }
 
 export default MyApp
