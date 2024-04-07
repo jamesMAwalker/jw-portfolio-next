992852f00457b6fe562e4396a80005264de51328
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2024-04-07 17:10:15 +0700

    fix: :bug: Fix contact button not working in mobile.

--- components/home/05-contact.jsx
+++ components/home/05-contact.jsx
@@ -1,35 +1,34 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
+
+import { DoubleMarquee } from '../project/double-marquee'
+import { EmailBtn, LPEmailBtn } from './email-btn'
 
 import {
   contact,
   label,
   banners,
-  bannerLine,
-  lineOne,
-  lineTwo,
-  phrase as phraseStyle,
   blurb,
 } from '../../styles/home/05-contact.module.scss'
-import { DoubleMarquee } from '../project/double-marquee'
-import { EmailBtn } from './email-btn'
 
 export const Contact = () => {
   // TODO - Fix email actions in mobile version.
+  const [isMobile, setIsMobile] = useState(false)
+
+  useEffect(() => {
+    setIsMobile(window.innerWidth < 768)
+  }, [])
 
   return (
     <section className={contact} id='contact'>
       <h3 className={label}>Contact</h3>
-      <a href='mailto:me@jmswlkr.dev' className={banners}>
-        <DoubleMarquee
-          words={['Get in Touch', '- Say Hello']}
-        />
+      <a href='mailto:me@jameswalker.dev' className={banners}>
+        <DoubleMarquee words={['Get in Touch', '- Say Hello']} />
       </a>
       <p className={blurb}>
-        <span>Currently available</span> for project work,
-        but also seeking fulltime employment. <br /> Either way,
-        let’s have a conversation.
+        <span>Currently available</span> for project work, but also seeking
+        fulltime employment. <br /> Either way, let’s have a conversation.
       </p>
-      <EmailBtn />
+      {isMobile ? <LPEmailBtn /> : <EmailBtn />}
     </section>
   )
 }
